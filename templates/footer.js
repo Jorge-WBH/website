fa4538182a095c0c4bf99f653b23d23601db9ecc
Mrzuuzzz <jorgejrwellesley@gmail.com>
2025-10-27 20:53:38 -0400

    updated resume

--- templates/footer.js
+++ templates/footer.js
@@ -30,7 +30,7 @@ class Footer extends HTMLElement {
             <div class="footer-content">
                 Jorge Wellesley-Bourke Hernandez<br>
                 Last updated on:<br>
-                October 26, 2025<br>
+                October 27, 2025<br>
             </div>
         </div>
         `;
